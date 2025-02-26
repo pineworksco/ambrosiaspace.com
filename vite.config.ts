8c84bbcefcc88c60e4ab4754f711816aed3eddbd
Ryan Irelan <ryan@irelan.net>
2025-02-26 16:47:50 -0600

    updated to use command to set proper base

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({command}) => ({
+    base: command === 'serve' ? '/' : '/dist/',
     build: {
         manifest: true,
         rollupOptions: {
@@ -17,8 +18,9 @@ export default defineConfig({
         cors: {
             origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(localhost|\.local|\.test|\.site)(?::\d+)?$/
         },
+        origin: "https://craftcms-vite-tailwind.ddev.site:5173",
         watch: {
             ignored: ['./storage/**', './vendor/**', './web/**'],
         }
     }
-});
+}));
