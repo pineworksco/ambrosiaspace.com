baa78a3fa2ef2bb20b110e7e7998c70ebe60c96e
Ryan Irelan <ryan@irelan.net>
2025-02-26 17:37:23 -0600

    added vite compression and vite restart plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import ViteRestart from 'vite-plugin-restart';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig(({command}) => ({
     base: command === 'serve' ? '/' : '/dist/',
-    plugins: [tailwindcss()],
+    plugins: [
+        tailwindcss(),
+        ViteRestart(
+            {
+                restart: ['./templates/**/*']
+            }
+       ),
+        viteCompression({
+            filter: /\.(mjs|json|css|map)$/i,
+        }),
+    ],
     build: {
         manifest: true,
         rollupOptions: {
