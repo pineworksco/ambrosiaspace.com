20f11d0c01bb47c864cdb7393a02eab990c0e91c
Ryan Irelan <ryan@irelan.net>
2025-02-28 15:32:34 -0600

    simplified the app.js file to remove sample code and add in HMR code

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 import '../css/app.css';
-import JSConfetti from 'https://cdn.jsdelivr.net/npm/js-confetti@0.12.0/+esm'
-document.addEventListener('DOMContentLoaded', () => {
-    const jsConfetti = new JSConfetti()
-    jsConfetti.addConfetti({
-        emojis: ['💩']
-    })
-});
+
+// Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
+if (import.meta.hot) {
+    import.meta.hot.accept(() => {
+        console.log("HMR")
+    });
+}
